2968cb0d874e68f62932117a2ec7445ea0cdc5c3
João Lenon <lenonsec7@gmail.com>
2021-07-08 00:28:14 -0300

    fix: host to mobile

--- lib/contants/app_api_urls.dart
+++ lib/contants/app_api_urls.dart
@@ -1,7 +1,7 @@
 class ApiUrls{
   static String prefix = "http://";
-  // static String host = "10.0.2.2:3000";
-  // static String hostqst = "10.0.2.2:3001";
-  static String host = "127.0.0.1:3000";
-  static String hostqst = "127.0.0.1:3001";
+  static String host = "10.0.2.2:3000";
+  static String hostqst = "10.0.2.2:3001";
+  // static String host = "127.0.0.1:3000";
+  // static String hostqst = "127.0.0.1:3001";
 }
