11cd0b92bb5b67e7dc2e3675e6a4e2c3e14868ea
GabrielUlisses <gulissessti@hotmail.com>
2021-07-08 22:08:18 -0300

    comentario component

--- lib/components/RespostaComponent.dart
+++ lib/components/RespostaComponent.dart
@@ -45,75 +45,79 @@ class _RespostaComponentState extends State<RespostaComponent> {
     Size size = MediaQuery.of(context).size;
 
     return Container(
-      width: double.infinity,
-      margin: EdgeInsets.only( left: 20, top: 6, right: 20, bottom: 6),
-      padding: EdgeInsets.only( left: 10, top: 4, right: 10, bottom: 4),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.only(top: 6),
-            child: Container(                 
-              child: Text(
-                this.widget.resposta.conteudo,
-                style: GoogleFonts.openSans( color: Color.fromRGBO(161,161,161,1), fontSize: 9, fontWeight: FontWeight.w600),
-                overflow: TextOverflow.clip,                   
-              ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only( left: 20, top: 6, right: 20, bottom: 6),
+            padding: EdgeInsets.only( left: 10, top: 4, right: 10, bottom: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 6),
+                  child: Container(                 
+                    child: Text(
+                      this.widget.resposta.conteudo,
+                      style: GoogleFonts.openSans( color: Color.fromRGBO(161,161,161,1), fontSize: 9, fontWeight: FontWeight.w600),
+                      overflow: TextOverflow.clip,                   
+                    ),
+                  ),
+                ),
+                Row(                 
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                        child: IconCount(count: this.widget.resposta.nrComentarions, icon: Icons.question_answer, size: 18, ),
+                        onTap: (){
+                          setState(() {
+                            this.comentariosVisible = !this.comentariosVisible;
+                          });
+                        }
+                    ),
+                    GestureDetector(
+                        child: IconCount(count: this.widget.resposta.nrAprovacoes, icon: Icons.thumb_up, size: 18, ),
+                        onTap: () {
+                          this.widget.resposta.reacaoResposta = true;
+
+                          this.service.updateResposta(id: this.widget.resposta.id, resposta: this.widget.resposta).then((response) => {
+                            setState(() {
+                              this.widget.resposta = response;
+                            })
+                          });
+                        }
+                    ),
+                    GestureDetector(
+                        child: IconCount(count: this.widget.resposta.nrDesaprovacoes, icon: Icons.thumb_down, size: 18 ),
+                        onTap: () {
+                          this.widget.resposta.reacaoResposta = false;
+
+                          this.service.updateResposta(id: this.widget.resposta.id, resposta: this.widget.resposta).then((response) => {
+                            setState(() {
+                              this.widget.resposta = response;
+                            })
+                          });
+                        }
+                    ),
+                  ],
+                ),              
+              ]
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: this.widget.resposta.resolveu == true ? Border(left: BorderSide(color: Colors.green[800], width: 8, style: BorderStyle.solid )) : null
+              ,boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(1,3),
+                  blurRadius: 3
+                )
+              ]
             ),
           ),
-          Row(                 
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              GestureDetector(
-                  child: IconCount(count: this.widget.resposta.nrComentarions, icon: Icons.question_answer, size: 18, ),
-                  onTap: (){
-                    setState(() {
-                      this.comentariosVisible = !this.comentariosVisible;
-                    });
-                  }
-              ),
-              GestureDetector(
-                  child: IconCount(count: this.widget.resposta.nrAprovacoes, icon: Icons.thumb_up, size: 18, ),
-                  onTap: () {
-                    this.widget.resposta.reacaoResposta = true;
-
-                    this.service.updateResposta(id: this.widget.resposta.id, resposta: this.widget.resposta).then((response) => {
-                      setState(() {
-                        this.widget.resposta = response;
-                      })
-                    });
-                  }
-              ),
-              GestureDetector(
-                  child: IconCount(count: this.widget.resposta.nrDesaprovacoes, icon: Icons.thumb_down, size: 18 ),
-                  onTap: () {
-                    this.widget.resposta.reacaoResposta = false;
-
-                    this.service.updateResposta(id: this.widget.resposta.id, resposta: this.widget.resposta).then((response) => {
-                      setState(() {
-                        this.widget.resposta = response;
-                      })
-                    });
-                  }
-              ),
-            ],
-          ),
-          Column(
-            children: this.comentariosVisible == true ? this.showComentarios() : [],
-          )
-        ]
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: this.widget.resposta.resolveu == true ? Border(left: BorderSide(color: Colors.green[800], width: 8, style: BorderStyle.solid )) : null
-        ,boxShadow: [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(1,3),
-            blurRadius: 3
-          )
-        ]
+          this.comentariosVisible == true ? this.showComentarios() : Text("")
+        ],
       ),
     );
   }
